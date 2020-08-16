664ed72bf8e30479cd531d0a5c50ccfe13367ebc
Ketan Chaudhari <ketan.dinesh.chaudhari@gmail.com>
2020-08-16 22:16:20 +0530

    Navigation Part 1

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Card } from 'react-native-elements';
-
+import {DISHES} from '../shared/dishes';
 
 function RenderDish(props){
     const dish = props.dish;
@@ -12,9 +12,9 @@ function RenderDish(props){
             featuredTitle={dish.name}
             image={require('./images/uthappizza.png')}
             >
-            <text style={{margin: 10}}>
+            <Text style={{margin: 10}}>
                 {dish.description}
-            </text>
+            </Text>
             </Card>
         );
     }
@@ -26,10 +26,26 @@ function RenderDish(props){
 }
 
 
-function Dishdetail(props){
-    return(
-         <RenderDish dish={props.dish} />
-    );
+class Dishdetail extends component{
+    
+constructor(props){
+    super(props);
+    this.state={
+        dishes: DISHES
+    };
+}
+
+static navigationOptions ={
+    title:'Dish Details'
+}
+
+render(){
+    const dishId= this.props.navigation.getParam('dishId','')
+
+        return(
+            <RenderDish dish={this.state.dishes[+dishId]} />
+       );
+    }
 }
 
 export default Dishdetail;
